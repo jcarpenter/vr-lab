c37200e496e365171aedb200706fe62fe3042465
Josh Carpenter <jcarpenter@mozilla.com>
2014-09-27 19:50:31 -0700

    Update 2014-09-28T02:50:31.048Z

--- js/scenes/mockups2.js
+++ js/scenes/mockups2.js
@@ -2,13 +2,14 @@ function mockups2() {
 
 	var radius = 50;	
 
+	var holder = new THREE.Object3D();
+
 	var mesh = new THREE.Mesh(
 		new THREE.PlaneGeometry( 180, 60, 1, 1 ),
 		new THREE.MeshBasicMaterial( { transparent: true, map: THREE.ImageUtils.loadTexture( 'images/mockups2/green1.png' ) } )
 	)
 
 	mesh.material.side = THREE.DoubleSide;
-	mesh.material.opacity = 0;
 	
 	holder.position.set( 0, 0, 0 - radius);
 
